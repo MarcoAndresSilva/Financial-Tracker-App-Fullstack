c63cf4d1476657e4b190eb6e33b19602238801c5
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-09-04 23:23:54 -0400

    chore: add shared material modules needed for layout

--- frontend/src/app/shared/material/material.module.ts
+++ frontend/src/app/shared/material/material.module.ts
@@ -3,6 +3,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 export const MATERIAL_MODULES = [
   MatButtonModule,
@@ -10,4 +13,7 @@ export const MATERIAL_MODULES = [
   MatInputModule,
   MatIconModule,
   MatCardModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule,
 ];
